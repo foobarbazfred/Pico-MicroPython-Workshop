542709747c50c66d8d6c67c105d5e1210804dd51
Fumio <160304755+foobarbazfred@users.noreply.github.com>
2025-08-12 23:36:08 +0900

    Update upysh.py

--- Foundation/src/lib/upysh.py
+++ Foundation/src/lib/upysh.py
@@ -3,9 +3,9 @@
 #     https://github.com/m5stack/M5Stack_MicroPython/blob/master/MicroPython_BUILD/components/micropython/esp32/modules/upysh.py
 #
 #
+
 import sys
 import os
-import time
 
 class LS:
 
@@ -14,68 +14,14 @@ class LS:
         return ""
 
     def __call__(self, path="."):
-        if path == "/":
-            l = os.listdir('/')
-            for f in l:
-                print("   <dir>  %s" % (f))
-            return
-
-        if path == ".":
-            path = os.getcwd()
-        try:
-            st = os.stat(path)
-        except:
-            print("Not a directory")
-            return
-            
-        if not (st[0] & 0x4000):
-            print("Not a directory")
-            return
-
-        origpath = os.getcwd()
-        os.chdir(path)
-        l = os.listdir()
+        l = os.listdir(path)
         l.sort()
-        total = 0
         for f in l:
-            st = os.stat(f)
-            #ftime = time.strftime("%Y/%m/%d %X", time.localtime(st[8])) # unavaiable MicroPython for RPi Pico
-            (YY, MM, DD, hh, mm, ss, _, _)time.localtime(st[8])
-            ftime = f'{YY}/{MM:02d}/{DD:02d} {hh:02d}:{mm:02d}:{ss:02d}'
+            st = os.stat("%s/%s" % (path, f))
             if st[0] & 0x4000:  # stat.S_IFDIR
-                print("   <dir>  %s  %s" % (ftime, f))
+                print("   <dir> %s" % f)
             else:
-                print("% 8d  %s  %s" % (st[6], ftime, f))
-                total += st[6]
-        drive = os.getdrive()
-        if drive != "/":
-            stvfs = os.statvfs(drive)
-            if total < (10*1024):
-                stotal = "{} B".format(total)
-            elif total < (1024*1024):
-                stotal = "{:0.2f} KB".format(total/1024)
-            else:
-                stotal = "{:0.3f} MB".format(total/(1024*1024))
-            print("\nTotal in '{}': {}".format(path, stotal))
-
-            dsize = stvfs[0]*stvfs[2]
-            dfree = stvfs[0]*stvfs[3]
-            if dsize < (1024*1024):
-                ssize = "{:0.2f} KB".format(dsize/(1024))
-            elif dsize < (1024*1024*1024):
-                ssize = "{:0.3f} MB".format(dsize/(1024*1024))
-            else:
-                ssize = "{:0.3f} GB".format(dsize/(1024*1024*1024))
-
-            if dfree < (1024*1024):
-                sfree = "{:0.2f} KB".format(dfree/(1024))
-            elif dsize < (1024*1024*1024):
-                sfree = "{:0.3f} MB".format(dfree/(1024*1024))
-            else:
-                sfree = "{:0.3f} GB".format(dfree/(1024*1024*1024))
-
-            print("Drive: '{}' size: {}, free: {}".format(drive, ssize, sfree))
-        os.chdir(origpath)
+                print("% 8d %s" % (st[6], f))
 
 class PWD:
 
@@ -92,59 +38,10 @@ class CLEAR:
     def __call__(self):
         return self.__repr__()
 
-class CP:
-
-    def __repr__(self):
-        self.__call__()
-        return ""
-
-    def __call__(self, srcf, destf):
-        if srcf == destf:
-            print("Destination file must be different than source")
-            return
-        try:
-            st = os.stat(srcf)
-        except:
-            print("'{}' not found".format(srcf))
-            return
-        try:
-            st = os.stat(destf)
-            print("'{}' exists".format(destf))
-            return
-        except:
-            pass
-        try:
-            fs = open(srcf, 'rb')
-            try:
-                fd = open(destf, 'wb')
-            except:
-                fs.close()
-                print("Error opening '{}'".format(srcf))
-                return
-            try:
-                while True:
-                    buf = fs.read(1024)
-                    if len(buf) == 0:
-                        break
-                    else:
-                        fd.write(buf)
-            except:
-                try:
-                    fd.close()
-                    os.remove(destf)
-                except:
-                    pass
-                print("Error during copy")
-            fd.close()
-            fs.close()
-        except:
-            print("Error opening '{}'".format(srcf))
-        
 
 pwd = PWD()
 ls = LS()
 clear = CLEAR()
-cp = CP()
 
 cd = os.chdir
 mkdir = os.mkdir
@@ -184,11 +81,9 @@ To see this help text again, type "man".
 
 upysh commands:
 pwd, cd("new_dir"), ls, ls(...), head(...), cat(...)
-newfile(...), mv("old", "new"), rm(...), mkdir(...), rmdir(...)
-cp("from", "to")
+newfile(...), mv("old", "new"), rm(...), mkdir(...), rmdir(...),
 clear
 """)
 
 man = Man()
-
 print(man)
