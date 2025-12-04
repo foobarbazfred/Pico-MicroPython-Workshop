ac414c1933ca84900a2a2fb414f6f78f91e2beee
Fumio <160304755+foobarbazfred@users.noreply.github.com>
2025-12-04 16:34:23 +0900

    Update boot.py

--- 3-Day1-Foundation/src/boot.py
+++ 3-Day1-Foundation/src/boot.py
@@ -7,30 +7,39 @@ import ntptime
 SSID = 'xxxxx24G'
 PASSWD = 'xxxxx'
 
+MAX_RETRY = 10
+station = None
+
 def setup_WiFi(id, pwd):
     station = network.WLAN(network.STA_IF)
     station.active(True)
     station.connect(id, pwd)
     while station.isconnected() == False:
-        pass
-
+        time.sleep(1)
+        retry_count += 1
+        if retry_count > MAX_RETRY:
+            print('Connection failed')
+            return station, False
     print('Connection successful')
     print(station.ifconfig())
+    return station, True
 
-#setup_WiFi(SSID, PASSWD)
+station, status = setup_WiFi(SSID, PASSWD)
 
-# Synchronize system clock using NTP
-# Wait briefly to ensure DNS is ready
-time.sleep(3)
+if status:
+    # Synchronize system clock using NTP
+    # Wait briefly to ensure DNS is ready
+    time.sleep(3)
 
-# Try synchronizing  system clock by  NTP
-#try:
-#    ntptime.settime()
-#    print("Time synchronized:", time.localtime())
-#except OSError as e:
-#    print("NTP sync failed:", e)
+    # Try synchronizing  system clock by  NTP
+    try:
+        ntptime.settime()
+        print("Time synchronized:", time.localtime())
+    except OSError as e:
+        print("NTP sync failed:", e)
 
-#from upysh import *
 #
+# import micropython shell
+#from upysh import *
 #
 #
